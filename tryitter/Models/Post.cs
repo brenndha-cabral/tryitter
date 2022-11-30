d6a817306fe858712ed1b624cc8137835e36951a
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-11-30 00:21:45 -0300

    feat: create Post and Student models

---
+++ tryitter/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tryitter.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string? Content { get; set; }
+        public DateTime Release { get; set; }
+        public int StudentId { get; set; }
+        [ForeignKey("StudentId")]
+        public Student? Student { get; set; }
+    }
+}
---
+++ tryitter/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tryitter.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Post>? Posts { get; set; }
+    }
+}
