d8228f322db7c6cb878e5bfec54d1be963a9801d
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-11-30 00:22:40 -0300

    feat: create Student context interface

---
+++ tryitter/Interfaces/IStudentsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tryitter.Models;
+
+namespace tryitter.Interfaces
+{
+    public interface IStudentsContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public int SaveChanges();
+    }
+}
