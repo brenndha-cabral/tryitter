c2d490a328b607d82a2556085ea50a8aebb75b41
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-11-30 00:22:46 -0300

    feat: create Student repository

---
+++ tryitter/Repository/StudentRepository.cs
@@ -0,0 +1,34 @@
+﻿using tryitter.Database;
+using tryitter.DTO;
+using tryitter.Interfaces;
+
+namespace tryitter.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        protected readonly StudentContext _context;
+
+        public StudentRepository(StudentContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<StudentDTO> GetStudents()
+        {
+            var teste = _context.Students
+                .Select(s => new StudentDTO
+                {
+                    StudentId = s.StudentId,
+                    Name = s.Name,
+                    Posts = s.Posts.Select(p => new PostDTO
+                    {
+                        Content = p.Content,
+                        Release = p.Release,
+                        PostId= p.PostId,
+                        StudentName = s.Name
+                    }).ToList()
+                });
+        }
+
+    }
+}
