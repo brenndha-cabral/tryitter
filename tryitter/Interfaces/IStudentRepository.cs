ef5f221ce71db4b1a1a56bbfadf6ba516056e168
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-11-30 00:22:29 -0300

    feat: create Student repository interface

---
+++ tryitter/Interfaces/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿using tryitter.DTO;
+
+namespace tryitter.Interfaces
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<StudentDTO> GetStudents();
+        StudentDTO GetStudentById(int id);
+        StudentDTO CreateStudent(StudentNameDTO post);
+        StudentDTO UpdateStudent(StudentNameDTO post, int postId);
+        void DeleteStudent(int id);
+    }
+}
