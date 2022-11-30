c1400e0b9003ec38076b19fdab074dbafa8ccbb2
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-11-30 00:21:09 -0300

    feat: create Post and Student DTOs

---
+++ tryitter/DTO/PostDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using tryitter.Models;
+
+namespace tryitter.DTO
+{
+    public class PostDTO
+    {
+        public int PostId { get; set; }
+        public string? Content { get; set; }
+        public DateTime Release { get; set; }
+        public string? StudentName { get; set; }
+    }
+
+    public class PostInsert
+    {
+        public string? Content { get; set; }
+        public string? StudentName { get; set; }
+    }
+}
---
+++ tryitter/DTO/StudentDTO.cs
@@ -0,0 +1,16 @@
+﻿using tryitter.Models;
+
+namespace tryitter.DTO
+{
+    public class StudentDTO
+    {
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public ICollection<PostDTO>? Posts { get; set; }
+    }
+
+    public class StudentNameDTO
+    {
+        public string? Name { get; set; }
+    }
+}
